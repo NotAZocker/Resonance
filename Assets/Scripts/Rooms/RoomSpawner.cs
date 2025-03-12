efc8ab657777ea51bcca05cc9ffebd59d62f8480
XenonnoneX <xeno.boecker@web.de>
2025-03-12 14:10:37 +0100

    connect rooms

--- Assets/Scripts/Rooms/RoomSpawner.cs
+++ Assets/Scripts/Rooms/RoomSpawner.cs
@@ -5,26 +5,4 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    [SerializeField] Transform roomParent;
-
-    [SerializeField] RoomController[] roomPrefabs;
-
-    public RoomController SpawnNewRandomRoom(RoomController attachRoom)
-    {
-        RoomController newRoom = roomPrefabs[Random.Range(0, roomPrefabs.Length)];
-
-        return SpawnNewRoom(attachRoom, newRoom);
-    }
-
-    public RoomController SpawnNewRoom(RoomController attachRoom, RoomController roomPrefab)
-    {
-        RoomController newRoom = Instantiate(roomPrefab, roomParent);
-
-        if (!attachRoom.TryAttachRoom(newRoom))
-        {
-            Debug.LogError("Can't attach room to " + attachRoom.name);
-        }
-
-        return newRoom;
-    }
 }
