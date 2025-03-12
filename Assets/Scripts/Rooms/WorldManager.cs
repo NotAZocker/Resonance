3921408d63aecc1cd13387542798f8cb382d0000
XenonnoneX <xeno.boecker@web.de>
2025-03-12 12:55:05 +0100

    wip

--- Assets/Scripts/Rooms/WorldManager.cs
+++ Assets/Scripts/Rooms/WorldManager.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class WorldManager : MonoBehaviour
 {
     [SerializeField] RoomController startRoom;
 
     RoomSpawner roomSpawner;
+    FirstPersonController player;
+
+    List<RoomController> rooms = new List<RoomController>();
 
     RoomController lastSpawnedRoom;
-    //FirstPersonController player;
 
     private void Awake()
     {
@@ -15,10 +20,35 @@ public class WorldManager : MonoBehaviour
         lastSpawnedRoom = startRoom;
     }
 
-
+    private void Start()
+    {
+        player = FindAnyObjectByType<FirstPersonController>();
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) lastSpawnedRoom = roomSpawner.SpawnNewRandomRoom(lastSpawnedRoom);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            lastSpawnedRoom = roomSpawner.SpawnNewRandomRoom(lastSpawnedRoom);
+            rooms.Add(lastSpawnedRoom);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ConnectRandomRooms();
+        }
+    }
+
+    private void ConnectRandomRooms()
+    {
+        int rand1 = UnityEngine.Random.Range(0, rooms.Count);
+        int rand2;
+        do
+        {
+            rand2 = UnityEngine.Random.Range(0, rooms.Count);
+        }
+        while (rand2 == rand1);
+
+
     }
 }
