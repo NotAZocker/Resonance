6268d12d49bc5394fd821c33d2f713e3be992cbc
XenonnoneX <xeno.boecker@web.de>
2025-03-14 15:13:32 +0100

    audio manager for music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicAudioSource;
+    [SerializeField] AudioClip[] musicClips;
+
+    List<AudioClip> shuffledClips;
+    int currentClipIndex = 0;
+    AudioClip lastPlayedClip;
+
+    void Start()
+    {
+        ShuffleMusicList();
+        PlayNextTrack();
+    }
+
+    void ShuffleMusicList()
+    {
+        List<AudioClip> tempClips = new List<AudioClip>(musicClips);
+        if (lastPlayedClip != null)
+        {
+            tempClips.Remove(lastPlayedClip);
+            tempClips = Shuffle(tempClips);
+            tempClips.Insert(Random.Range(1, tempClips.Count + 1), lastPlayedClip); // Ensures last played isn't first
+        }
+        else
+        {
+            tempClips = Shuffle(tempClips);
+        }
+        shuffledClips = tempClips;
+        currentClipIndex = 0;
+    }
+
+    List<AudioClip> Shuffle(List<AudioClip> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (list[i], list[randomIndex]) = (list[randomIndex], list[i]);
+        }
+        return list;
+    }
+
+    void PlayNextTrack()
+    {
+        if (currentClipIndex >= shuffledClips.Count)
+        {
+            ShuffleMusicList();
+        }
+
+        lastPlayedClip = shuffledClips[currentClipIndex];
+        musicAudioSource.clip = lastPlayedClip;
+        musicAudioSource.Play();
+        currentClipIndex++;
+
+        Invoke(nameof(PlayNextTrack), lastPlayedClip.length);
+    }
+}
