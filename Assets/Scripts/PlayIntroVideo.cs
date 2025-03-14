8d5b845b732ac28bf434b1dc7d3ff44b0d659e4a
XenonnoneX <xeno.boecker@web.de>
2025-03-14 21:01:06 +0100

    intro video

---
+++ Assets/Scripts/PlayIntroVideo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayIntroVideo : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer;
+    [SerializeField] GameObject uiPanel;
+
+    void Start()
+    {
+        Time.timeScale = 0; // Pause the game
+        uiPanel.SetActive(true);
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += OnVideoPrepared;
+    }
+
+    void OnVideoPrepared(VideoPlayer vp)
+    {
+        videoPlayer.Play();
+        videoPlayer.prepareCompleted -= OnVideoPrepared; // Unsubscribe to prevent memory leaks
+        videoPlayer.loopPointReached += OnVideoFinished; // Event when video ends
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        Time.timeScale = 1; // Resume game
+        vp.loopPointReached -= OnVideoFinished; // Unsubscribe event
+        uiPanel.SetActive(false);
+        Destroy(gameObject); // Destroy this script
+    }
+}
