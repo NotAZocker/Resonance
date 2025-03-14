2fcd22bc63587503db103c9578acf1d27b110447
XenonnoneX <xeno.boecker@web.de>
2025-03-14 23:21:17 +0100

    intro fade and cam pan

--- Assets/Scripts/PlayIntroVideo.cs
+++ Assets/Scripts/PlayIntroVideo.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿using ECM2.Walkthrough.Ex52;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class PlayIntroVideo : MonoBehaviour
 {
     [SerializeField] VideoPlayer videoPlayer;
-    [SerializeField] GameObject uiPanel;
+    [SerializeField] CanvasGroup uiPanel;
+    [SerializeField] RawImage videoImage;
+
+    [SerializeField] float fadeTime = 0.5f;
+
+    [SerializeField] Transform playerCam;
+    FirstPersonController playerController;
+    [SerializeField] float camPanUpTime = 0.5f;
+    [SerializeField] float camStartAngle = 45, camEndAngle = 0;
+
+    [Header("For Testing: Security Mode")]
     [SerializeField] bool securityMode = true;
     [SerializeField] bool skipVideo = false;
 
@@ -22,13 +34,18 @@ public class PlayIntroVideo : MonoBehaviour
             return;
         }
 
+        Debug.Log("Start Video");
+
         Time.timeScale = 0; // Pause the game
-        uiPanel.SetActive(true);
+        uiPanel.gameObject.SetActive(true);
         videoPlayer.Prepare();
         videoPlayer.prepareCompleted += OnVideoPrepared;
         securityTimer = videoPlayer.clip.length + 1; // Add 1 second to the video length
 
-        if(securityMode) securityCoroutine = StartCoroutine(IfBrokenStartGame());
+        playerController = playerCam.GetComponentInParent<FirstPersonController>();
+        playerController.enabled = false;
+
+        if (securityMode) securityCoroutine = StartCoroutine(IfBrokenStartGame());
     }
 
     void OnVideoPrepared(VideoPlayer vp)
@@ -54,9 +71,7 @@ public class PlayIntroVideo : MonoBehaviour
             StopCoroutine(securityCoroutine);
         }
 
-        Time.timeScale = 1; // Resume game
-        uiPanel.SetActive(false);
-        Destroy(gameObject); // Destroy this script
+        StartCoroutine(StartGameCoroutine());
     }
 
     IEnumerator IfBrokenStartGame()
@@ -70,5 +85,42 @@ public class PlayIntroVideo : MonoBehaviour
         {
             StartGame();
         }
+    }
+
+    IEnumerator StartGameCoroutine()
+    {
+        Debug.Log("StartGame Coroutine");
+        for (float i = 0; i < fadeTime; i+= Time.unscaledDeltaTime)
+        {
+            uiPanel.alpha = 1 - (i / fadeTime);
+
+            yield return null;
+        }
+
+        uiPanel.alpha = 0;
+        uiPanel.blocksRaycasts = false;
+
+        videoImage.gameObject.SetActive(false);
+
+        playerCam.transform.rotation = Quaternion.Euler(camStartAngle, playerCam.rotation.eulerAngles.y, playerCam.rotation.eulerAngles.z);
+
+        for (float i = 0; i < camPanUpTime; i += Time.unscaledDeltaTime)
+        {
+            float angle = Mathf.Lerp(camStartAngle, camEndAngle, i / camPanUpTime);
+
+            playerCam.transform.rotation = Quaternion.Euler(angle, playerCam.rotation.eulerAngles.y, playerCam.rotation.eulerAngles.z);
+
+            yield return null;
+        }
+        Debug.Log("Start Game");
+
+        playerCam.transform.rotation = Quaternion.Euler(camEndAngle, playerCam.rotation.eulerAngles.y, playerCam.rotation.eulerAngles.z);
+
+        if(playerController != null) playerController.enabled = true;
+
+        Time.timeScale = 1; // Resume game
+
+        uiPanel.gameObject.SetActive(false);
+        Destroy(gameObject); // Destroy this script
     }
 }
