e79f041ed89f5c2ebee9dbc7c04db0b120166f34
CEntertain <TimGeru@gmx.de>
2025-03-15 02:37:42 +0100

    Removed Volumetric Lighting Package

--- Assets/Scripts/Weapon/FlashlightLerp.cs
+++ Assets/Scripts/Weapon/FlashlightLerp.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FlashlightLerp : MonoBehaviour
 {
     [SerializeField] private Transform target; // Assign the player or camera transform here
     [SerializeField] private float lerpSpeed = 5f;
+    [SerializeField] private Vector3 transformOffset;
+
+    private Light flashlight;
+
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+        flashlight.enabled = false;
+    }
 
     void Update()
     {
@@ -11,5 +21,12 @@ public class FlashlightLerp : MonoBehaviour
 
         // Smoothly interpolate the flashlight's forward direction towards the target's forward direction
         transform.forward = Vector3.Lerp(transform.forward, target.forward, Time.deltaTime * lerpSpeed);
+
+        transform.position = target.transform.position + transformOffset;
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled; // yuh
+        }
     }
 }
