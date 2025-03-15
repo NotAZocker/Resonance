8a212702932ff1881ece2dd9724a9b7dc21434db
XenonnoneX <xeno.boecker@web.de>
2025-03-15 04:32:01 +0100

    notes and Menu fixed

--- Assets/Scripts/Interaction/NoteInteraction.cs
+++ Assets/Scripts/Interaction/NoteInteraction.cs
@@ -21,13 +21,27 @@ public class NoteInteraction : Interactable
         Material tempMat = new Material(paperMaterial);
         tempMat.name = "mat_" + count++;
         tempMat.SetTexture("_BaseMap", noteTex);
+
+        // Set transparency
+        SetMaterialTransparent(tempMat);
+
         meshRenderer.material = tempMat;
     }
 
     public override void Interact()
     {
         UIManager.Instance.ShowNote(noteSprite);
-
         base.Interact();
+    }
+
+    void SetMaterialTransparent(Material mat)
+    {
+        mat.SetFloat("_Surface", 1); // 1 = Transparent (URP)
+        mat.SetOverrideTag("RenderType", "Transparent");
+        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        mat.SetInt("_ZWrite", 0);
+        mat.EnableKeyword("_ALPHABLEND_ON");
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
     }
 }
